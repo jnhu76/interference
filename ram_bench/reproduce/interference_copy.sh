afc3a30283aae00912ecc818ac4a474028e07f0c
bxtx999 <fred1653@qq.com>
2021-12-17 11:00:03 +0800

    更新参数

--- ram_bench/reproduce/interference_copy.sh
+++ ram_bench/reproduce/interference_copy.sh
@@ -2,15 +2,15 @@ clang++ -g0 -O3 -DNDEBUG=1 -march=native -std=c++17 interference/interference.cp
 
 # Seqential ram
 echo "" > results/interference_seq_ram.txt
-./a.out 10  0  | tee -a results/interference_seq_ram.txt
-./a.out 10  1  | tee -a results/interference_seq_ram.txt
-./a.out 10  5  | tee -a results/interference_seq_ram.txt
-./a.out 10  10  | tee -a results/interference_seq_ram.txt
+./a.out 20  0  | tee -a results/interference_seq_ram.txt
+./a.out 20  1  | tee -a results/interference_seq_ram.txt
+./a.out 20  10  | tee -a results/interference_seq_ram.txt
+./a.out 20  20  | tee -a results/interference_seq_ram.txt
 
 
 # Seqential nvm
 echo "" > results/interference_seq_nvm.txt
-./a.out  0 10  | tee -a results/interference_seq_nvm.txt
-./a.out  1 10  | tee -a results/interference_seq_nvm.txt
-./a.out  5 10  | tee -a results/interference_seq_nvm.txt
-./a.out  10 10  | tee -a results/interference_seq_nvm.txt
+./a.out  0 20  | tee -a results/interference_seq_nvm.txt
+./a.out  1 20  | tee -a results/interference_seq_nvm.txt
+./a.out  10 20  | tee -a results/interference_seq_nvm.txt
+./a.out  20 20  | tee -a results/interference_seq_nvm.txt
